6be2ef47b6832729dced907c2c61fcf5936ed99a
AhmedSewidan <159721117+AhmedSewidan@users.noreply.github.com>
2024-08-12 01:31:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 header("Location: Dashboards/login/customerLogin.php"); //header to go to login page
-=======
-header("Location: Dashboards/login/customerLogin.php");
->>>>>>> 339361c782407a19314c13bffcc859ea3eeeabe4
+
 die();
